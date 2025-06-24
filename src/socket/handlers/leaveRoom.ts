effb84ad7093953ce2a42bb8a9147bf8e330c352
Dodul01 <mhdodul299@gmail.com>
2025-06-24 06:24:35 +0600

    Leave room half done

--- src/socket/handlers/leaveRoom.ts
+++ src/socket/handlers/leaveRoom.ts
@@ -1,2 +1,11 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
+import onlineUsers from "../onlineUsers";
 
+export const disconnectHandler = (_io: Server, socket: Socket) => () =>{
+    const userId = socket.data.userId;
+
+    if(userId){
+        console.log(`User disconnected: ${userId}`);
+        onlineUsers.delete(userId);
+    }
+}

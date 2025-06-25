f19e941a24c3cb2c98b3fd086e299a2a227dc522
Dodul01 <mhdodul299@gmail.com>
2025-06-25 06:03:51 +0600

    fail problem

--- src/models/User/user.ts
+++
@@ -1,4 +0,0 @@
-export type IVerifyEmail = {
-    email: string;
-    oneTimeCode: number;
-}
--- src/utils/generateOTP.ts
+++
@@ -1,3 +0,0 @@
-export const generateOTP = (length: number) : number => {
-	return Number(Math.floor(100000 + Math.random() * 900000).toString().slice(0, length));
-}
